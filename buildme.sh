b46f43f659dee09965eef350480edee198e054ad
maximevince <maxime.vince@gmail.com>
2015-12-24 13:30:15 +0100

    Minor update to buildme.sh

--- buildme.sh
+++ buildme.sh
@@ -3,6 +3,21 @@ rm -f build
 mkdir -p build
 cd build
 mkdir -p lib
+
 ../configure --prefix=`pwd`/lib --target=arm-none-eabi --disable-multilib --disable-newlib-supplied-syscalls CFLAGS_FOR_TARGET="-g -O2 -fPIC -mword-relocations -mlong-calls -fno-common -msingle-pic-base -mpic-register=r9 -mno-pic-data-is-text-relative"
+if [ $? -ne 0 ]; then
+    echo Failed to configure newlib
+    exit 1
+fi
+
 make CFLAGS_FOR_TARGET="-g -O2 -fPIC -mword-relocations -mlong-calls -fno-common -msingle-pic-base -mpic-register=r9 -mno-pic-data-is-text-relative"
+if [ $? -ne 0 ]; then
+    echo Failed to build newlib
+    exit 1
+fi
+
 make install
+if [ $? -ne 0 ]; then
+    echo Failed to install newlib
+    exit 1
+fi
