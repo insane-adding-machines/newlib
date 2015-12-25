e1ec48d20adf55619c6bd0ccbd122255516d1952
Maxime Vincent <maxime.vince@gmail.com>
2015-12-25 14:47:22 +0100

    Add cortex-m3 CFLAGS

--- buildme.sh
+++ buildme.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 rm -f build
+rm -f arm-none-eabi
 mkdir -p build
 cd build
 mkdir -p lib
 
-../configure --prefix=`pwd`/lib --target=arm-none-eabi --disable-multilib --disable-newlib-supplied-syscalls CFLAGS_FOR_TARGET="-g -O2 -fPIC -mword-relocations -mlong-calls -fno-common -msingle-pic-base -mpic-register=r9 -mno-pic-data-is-text-relative"
+CFLAGS_FOR_TARGET="-g -Os -mcpu=cortex-m3 -mthumb -mlittle-endian -mthumb-interwork -fPIC -mlong-calls -fno-common -msingle-pic-base -mno-pic-data-is-text-relative"
+export CFLAGS_FOR_TARGET
+
+../configure --prefix=`pwd`/lib --target=arm-none-eabi --disable-multilib --disable-newlib-supplied-syscalls --enable-newlib-nano-formatted-io --with-mode=thumb --with-cpu=cortex=m3
 if [ $? -ne 0 ]; then
     echo Failed to configure newlib
     exit 1
 fi
 
-make CFLAGS_FOR_TARGET="-g -O2 -fPIC -mword-relocations -mlong-calls -fno-common -msingle-pic-base -mpic-register=r9 -mno-pic-data-is-text-relative"
+make
 if [ $? -ne 0 ]; then
     echo Failed to build newlib
     exit 1
