e56c8a46eb8418570a22184372bef4bcb67e69b9
Daniele Lacamera <root@danielinux.net>
2016-03-06 13:55:08 +0100

    Flush buffered I/O upon exit()

--- newlib/libc/sys/frosted/exit.c
+++ newlib/libc/sys/frosted/exit.c
@@ -5,14 +5,20 @@
 #include "frosted_api.h"
 #include "syscall_table.h"
 #include <errno.h>
+#include <stdio.h>
+
 extern int sys_exit(int val);
 
 int exit(int val)
 {
+    fflush(stderr);
+    fflush(stdout);
     return sys_exit(val);
 }
 
 int _exit(int val) {
+    fflush(stderr);
+    fflush(stdout);
     return sys_exit(val);
 }
 
