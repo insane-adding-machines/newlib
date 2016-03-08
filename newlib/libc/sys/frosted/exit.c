5e340ecbe28187d8aac78ae51788841fa1f2ac8a
Daniele Lacamera <root@danielinux.net>
2016-03-08 15:32:53 +0100

    Cleaning up i/o buffers on exit()

--- newlib/libc/sys/frosted/exit.c
+++ newlib/libc/sys/frosted/exit.c
@@ -8,12 +8,23 @@
 
 extern int sys_exit(int val);
 
+void buf_close(void) {
+    fclose(stdin);
+    fclose(stdout);
+    fclose(stderr);
+    stdin = NULL;
+    stdout = NULL;
+    stderr = NULL;
+}
+
 int exit(int val)
 {
+    buf_close();
     return sys_exit(val);
 }
 
 int _exit(int val) {
+    buf_close();
     return sys_exit(val);
 }
 
