3d7bc7ff30602ef9d0e8ef4cf854d86a231adb9c
Daniele Lacamera <root@danielinux.net>
2016-03-11 21:09:03 +0100

    Added 3 grp calls

--- newlib/libc/sys/frosted/grp.c
+++ newlib/libc/sys/frosted/grp.c
@@ -7,9 +7,45 @@
 #include "grp.h"
 #include <errno.h>
 
+static const char gr_name[] = "root";
+const char *gr_members[2] = { gr_name, NULL };
+
+static struct group grp_root = { 
+    .gr_name = gr_name,
+    .gr_passwd = NULL,
+    .gr_gid = 0,
+    .gr_mem = gr_members
+};
+
+
 int getgroups(int size, gid_t list[])
 {
-    /* always fail for now */
-    errno = EPERM;
-    return -1;
+    if (size < 1) {
+        errno = EINVAL;
+        return -1;
+    }
+    list[0] = 0;
+    return 1;
+}
+
+
+struct group *getgrnam(const char *name) 
+{
+    if (!name) {
+        errno = EBADF;
+        return NULL;
+    }
+    if (strcmp(name, "root") != 0) {
+        errno = ENOENT;
+        return NULL;
+    }
+    return &grp_root;
+}
+
+struct group *getgrgid(gid_t g)
+{
+    if (g == 0)
+        return &grp_root;
+    errno = ENOENT;
+    return NULL;
 }
