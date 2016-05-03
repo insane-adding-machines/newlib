899b570c2568f2ebb415acc590fbe210289bc95c
Daniele Lacamera <root@danielinux.net>
2016-05-03 17:07:22 +0200

    Force sleep/usleep to sleep at least 1ms

--- newlib/libc/sys/frosted/sleep.c
+++ newlib/libc/sys/frosted/sleep.c
@@ -8,7 +8,7 @@ extern int sys_sleep(int ms);
 
 int sleep(int s)
 {
-    int ret = sys_sleep(s * 1000);
+    int ret = sys_sleep((s * 1000) + 1);
     if (ret < 0) {
         errno = 0 - ret;
         ret = -1;
@@ -18,7 +18,7 @@ int sleep(int s)
 
 int usleep(int us)
 {
-    int ret = sys_sleep(us / 1000);
+    int ret = sys_sleep((us / 1000) + 1);
     if (ret < 0) {
         errno = 0 - ret;
         ret = -1;
