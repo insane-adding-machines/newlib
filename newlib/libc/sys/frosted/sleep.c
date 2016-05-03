c942f7421a1db7cf3a15accd40efb42d59db131f
Daniele Lacamera <root@danielinux.net>
2016-05-03 16:43:09 +0200

    Added usleep

--- newlib/libc/sys/frosted/sleep.c
+++ newlib/libc/sys/frosted/sleep.c
@@ -4,11 +4,21 @@
 
 #include "syscall_table.h"
 #include <errno.h>
-extern int sys_sleep(int s);
+extern int sys_sleep(int ms);
 
 int sleep(int s)
 {
-    int ret = sys_sleep(s);
+    int ret = sys_sleep(s * 1000);
+    if (ret < 0) {
+        errno = 0 - ret;
+        ret = -1;
+    }
+    return ret;
+}
+
+int usleep(int us)
+{
+    int ret = sys_sleep(us / 1000);
     if (ret < 0) {
         errno = 0 - ret;
         ret = -1;
