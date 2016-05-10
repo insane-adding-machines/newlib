e2d5c7ec85f02f3762e98deea7cc5e1dc3c3fe03
brabo <brabo.sil@gmail.com>
2016-05-10 22:24:50 +0200

    install_to.sh: removed sudo from cp commands

--- install_to.sh
+++ install_to.sh
@@ -6,6 +6,6 @@ else
     TOOLCHAIN=$1
 fi
 
-sudo cp -av build/lib/arm-frosted-eabi/include/* $TOOLCHAIN/arm-frosted-eabi/include/
-sudo cp build/lib/arm-frosted-eabi/lib/* $TOOLCHAIN/arm-frosted-eabi/lib/ 
+cp -av build/lib/arm-frosted-eabi/include/* $TOOLCHAIN/arm-frosted-eabi/include/
+cp build/lib/arm-frosted-eabi/lib/* $TOOLCHAIN/arm-frosted-eabi/lib/
 
