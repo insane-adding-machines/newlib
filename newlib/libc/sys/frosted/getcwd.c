47a6d2e04165e7e40e58d7896ba694d5c9db84cd
Daniele Lacamera <root@danielinux.net>
2016-05-16 11:46:35 +0200

    Fixed getcwd()

--- newlib/libc/sys/frosted/getcwd.c
+++ newlib/libc/sys/frosted/getcwd.c
@@ -9,9 +9,8 @@ extern char * sys_getcwd(char *, int);
 char *getcwd(char *buf, int size)
 {
     int ret = sys_getcwd(buf, size);
-    if (ret < 0) {
-        errno = 0 - ret;
-        ret = -1;
+    if (ret == 0) {
+        errno = EINVAL;
     }
-    return ret;
+    return (char *)ret;
 }
