fdda742c5e418174aec14001c8785ce520e67826
brabo <brabo@cryptolab.net>
2017-03-07 00:41:24 +0100

    sem_timedwait libcall added, insane-adding-machines/frosted#119

--- newlib/libc/sys/frosted/include/semaphore.h
+++ newlib/libc/sys/frosted/include/semaphore.h
@@ -4,6 +4,8 @@
 typedef struct __frosted_libc_semaphore { int __reserved[5] } sem_t;
 typedef sem_t mutex_t;
 
+#include <sys/time.h>
+
 mutex_t *mutex_init(void);
 int mutex_unlock(mutex_t *s);
 int mutex_lock(mutex_t *s);
@@ -12,6 +14,7 @@ int mutex_destroy(mutex_t *s);
 int sem_init(sem_t *s, int pshared, int val);
 int sem_post(sem_t *s);
 int sem_wait(sem_t *s);
+int sem_timedwait(sem_t *s, struct timespec *t);
 int sem_trywait(sem_t *s);
 int sem_destroy(sem_t *s);
 
--- newlib/libc/sys/frosted/semaphore.c
+++ newlib/libc/sys/frosted/semaphore.c
@@ -5,8 +5,10 @@
 #include "sys/frosted.h"
 #include <errno.h>
 #include <semaphore.h>
+#include <sys/time.h>
+#include <stddef.h>
 extern int sys_sem_post(sem_t *s);
-extern int sys_sem_wait(sem_t *s);
+extern int sys_sem_wait(sem_t *s, struct timespec *t);
 extern int sys_sem_trywait(sem_t *s);
 extern int sys_sem_destroy(sem_t *s);
 
@@ -22,7 +24,17 @@ int sem_post(sem_t *s)
 
 int sem_wait(sem_t *s)
 {
-    int ret = sys_sem_wait(s);
+    int ret = sys_sem_wait(s, NULL);
+    if (ret < 0) {
+        errno = 0 - ret;
+        ret = -1;
+    }
+    return ret;
+}
+
+int sem_timedwait(sem_t *s, struct timespec *t)
+{
+    int ret = sys_sem_wait(s, t);
     if (ret < 0) {
         errno = 0 - ret;
         ret = -1;
